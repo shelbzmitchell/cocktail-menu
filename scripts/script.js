85228055f60a56ba7066f76ae0c33c98a2b428ac
Shelby Mitchell <shelbzmitchell@gmail.com>
2020-02-05 15:45:03 -0800

    basic styling, javascript generates list

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,26 @@ form.addEventListener("submit", submitEvent => {
     )
 
     .then(response => {
-      console.log(response);
+      getList(response.data.drinks);
+    })
+
+    .catch(reject => {
+      let noResults = document.createElement("p");
+      noResults.classList.add("list__item");
+      listSection.appendChild(noResults);
+      noResults.innerHTML =
+        "Sorry! We don't have any drinks with that ingredient";
     });
 });
+
+let listSection = document.querySelector(".list");
+function getList(list) {
+  listSection.innerHTML = "";
+  list.forEach(item => {
+    let drink = document.createElement("p");
+    drink.classList.add("list__item");
+    listSection.appendChild(drink);
+    drink.innerText = `${item["strDrink"]}`;
+    form.reset();
+  });
+}
